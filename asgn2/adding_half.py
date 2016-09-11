44bb8772dfa88c047346b78bbaa8a1fb8bc5271d
Alexander Oleinik <alxndr@stormtrooper.vrmnet>
2016-09-10 22:20:48 -0400

    getting started on asgn2c

---
+++ asgn2/adding_half.py
@@ -0,0 +1,26 @@
+# w2c_addinghalf.py
+
+from math import inf
+
+def number_from_half(s : str):
+    """return the number represented by s, a binary16 stored as a 4-character hex number"""
+    try:
+        i = int(s, 16)
+    except:
+        return 0
+    
+    return int(s, 16)
+
+def main():
+    """add all binary16 numbers from standard input until a non-number is entered, then print the total.
+    Numbers are represented in 4-character hex string format, one per line"""
+    sum = 0;
+    while(
+
+
+    # hint1: use int(input(),16)
+    # hint2: use try: except: to halt
+
+
+if __name__ == '__main__':
+    main()
