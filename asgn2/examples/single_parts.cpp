e51a721ccc57f9b1ceba3492d9d91e5292fee5f1
Alexander Oleinik <alxndr@solo.vrmnet>
2016-09-15 08:45:36 -0400

    w2b modified to work

--- asgn2/examples/single_parts.cpp
+++ asgn2/examples/single_parts.cpp
@@ -1,23 +1,9 @@
-// AUTHOR alexander.oleinik alxndr@bu.edu
 // single_parts
 #include <iostream>
 #include <iomanip>
 
 using namespace std;
 
-
-typedef unsigned int raw32; 
-
-
-struct Single_Parts {
-	raw32 fraction : 23;
-	raw32 exponent : 8;
-	raw32 sign : 1;
-};
-
-const raw32 MASK_SIGN = 1U << 31;
-const raw32 MASK_BEXP = 0xffU << 23;
-const raw32 MASK_FRAC = 0x7fffffU;
 // print out the parts of the structure Single_Parts
 void print_sp(Single_Parts sp) 
 { 
@@ -33,28 +19,14 @@ void print_sp(Single_Parts sp)
       << dec;
 }
 
-Single_Parts take_apart(float d)
-{
-	Single_Parts sp;
-	raw32 x = *reinterpret_cast<raw32*>(&d);
-	sp.sign = (x & MASK_SIGN) >> 31;
-	sp.exponent = (x & MASK_BEXP) >> 23;
-	sp.fraction = (x & MASK_FRAC);
-	return sp;
-}
-
-float build(Single_Parts sp)
-{
-	return *reinterpret_cast<float*>(&sp);
-}
-
 // define Single_Parts, build(), and take_apart() for float
+
 int main()
 {
-        int EXAMPLES = 5;
-	float num_from_build;
 
-	float numbers[EXAMPLES]={1.0/3,2,1.3e10,3e11,6};
+	double num_from_build;
+
+	double numbers[EXAMPLES]={1.0/3,2,1.3e10,3e11,6};
 	
 	// show the structure of the numbers 
 	for (int i=0;i<EXAMPLES;i++)
