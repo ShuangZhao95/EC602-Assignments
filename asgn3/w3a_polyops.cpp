dc8d96fed873d57abf3392efd404567d708bf10d
Oleinik <alxndr@vlsi57.bu.edu>
2016-09-15 18:34:02 -0400

    started asgn3

---
+++ asgn3/w3a_polyops.cpp
@@ -0,0 +1,14 @@
+typedef vector<double> Poly;
+
+// Add two polynomials, returning the result
+Poly add_poly(const Poly, const Poly);
+
+// // Multiply two polynomials, returning the result.
+Poly multiply_poly(const Poly, const Poly);
+
+
+Poly add_poly(const Poly &a, const Poly &b)
+{
+}
+
+Poly multiply_poly(const Poly &a, const Poly &b);
