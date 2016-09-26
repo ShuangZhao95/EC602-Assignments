833fa44022fcc162048237c67f23feb492ce5b03
Alexander Oleinik <alxndr@alxndr-mbp>
2016-09-26 14:15:10 -0400

    Added getter, setter, printer and constructor

--- asgn4/w4_polynomial.py
+++ asgn4/w4_polynomial.py
@@ -2,7 +2,25 @@
 
 
 class Polynomial():
-	def __init__ 
+    'Polynomial class with support for negative numbers'
+    def __init__(self, seq):
+        # Constructor - seq are coefficients ordered by exponent - high to low
+        self.poly = dict()
+        inc = 0
+        for c in reversed(seq):
+            self[inc] = c
+            inc = inc + 1
+    
+    def __getitem__(self,index):
+        return self.poly[index]
+
+    def __setitem__(self, key, value):
+        self.poly[key]=value
+
+    def __str__(self):
+        return str(self.poly)
+
+
 
 def main():
     pass
