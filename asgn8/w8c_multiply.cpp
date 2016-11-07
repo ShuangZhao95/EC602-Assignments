89fbbb7292cd26fcdee26097920f5a07b0426638
Alexander Oleinik <alxndr@solo.vrmnet>
2016-11-07 17:25:16 -0500

    updated c++

--- asgn8/w8c_multiply.cpp
+++ asgn8/w8c_multiply.cpp
@@ -1,4 +1,4 @@
-// AUTHOR alexander oleinik alxndr@bu.edu
+// AUTHOR alejandro aaparici@bu.edu
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
@@ -10,15 +10,20 @@
 typedef std::vector<std::vector<int> > intmatrix;
 typedef  std::vector<std::vector<double> > doublematrix;
 enum mult_mode {SQUARE, GENERAL};
-enum error_code {ERR_ARGS, ERR_READ, ERR_SHAPE, ERR_WRITE};
 
-int matrix_multiply(const intmatrix &m1, const intmatrix &m2, intmatrix* m3)
+#define ERR_ARGS 1
+#define ERR_READ 2
+#define ERR_SHAPE 3
+#define ERR_WRITE 4
+
+intmatrix matrix_multiply(const intmatrix &m1, const intmatrix &m2)
 {
-    m3->resize(m1.size(), std::vector<int>(m2[0].size()));
+    intmatrix m3(m1.size(), std::vector<int>(m2[0].size()));
     for (int i=0;i<m1.size();i++)
         for (int j=0;j<m2[0].size();j++)
             for (int k=0;k<m1[0].size();k++)
                 m3[i][j] += m1[i][k] * m2[k][j];
+    return m3;
 }
 
 doublematrix matrix_multiply(const doublematrix &m1, const doublematrix &m2)
@@ -32,28 +37,40 @@ doublematrix matrix_multiply(const doublematrix &m1, const doublematrix &m2)
 }
 
 
-int iread_matrix_from_file(std::string& fn, int& m, int& n, intmatrix* ma)
+intmatrix iread_matrix_from_file(std::string& fn, int& m, int& n)
 {
     std::ifstream file; // note: this is an IFSTREAM, "I" stands for INPUT
     file.open(fn.c_str());
-    if(!file) return -1;
-    ma->resize(m, std::vector<int>(n));
+    if(!file) throw ERR_READ;
+    intmatrix ma(m, std::vector<int>(n));
     int count = 0;
-    while(file >> ma[count/n][count%n])
-        count ++;
+    int i =0;
+    while(file >> i)
+    {
+        if(count < m*n)ma[count/n][count%n] = i;
+        else throw ERR_SHAPE;
+        count++;
+    }
     file.close();
-
+    if(count != m*n) throw ERR_SHAPE;
+    return ma;
 }
 doublematrix dread_matrix_from_file(std::string& fn, int& m, int& n)
 {
     std::ifstream file; // note: this is an IFSTREAM, "I" stands for INPUT
     file.open(fn.c_str());
+    if(!file) throw ERR_READ;
     doublematrix ma(m, std::vector<double>(n));
     int count = 0;
-    while(file >> ma[count/n][count%n])
-        count ++;
+    double i =0;
+    while(file >> i)
+    {
+        if(count < m*n)ma[count/n][count%n] = i;
+        else throw ERR_SHAPE;
+        count++;
+    }
     file.close();
-
+    if(count != m*n) throw ERR_SHAPE;
     return ma;
 }
 
@@ -61,11 +78,11 @@ template<typename T> int write_matrix_to_file(const char* fn, T m)
 {
     std::ofstream file; // note: this is an IFSTREAM, "I" stands for INPUT
     file.open(fn);
-    if(!file) return -1;
+    if(!file) throw ERR_WRITE;
     for (int i=0;i<m.size();i++)
     {
         for (int j=0;j<m[0].size();j++)
-                file << m[i][j] << " ";
+            file << m[i][j] << " ";
         file << std::endl;    
     }
     file.close();
@@ -77,9 +94,12 @@ int main(int argc, char const *argv[])
     bool dtype;
     std::string file1, file2, file3;
     int L, M, N;
-    dtype = std::string(argv[1]) == "double" ? true : false;
     if(argc == 6)
     {
+        if(std::string(argv[1]) == "double") dtype = true;
+        else if(std::string(argv[1]) == "int") dtype = false;
+        else return ERR_ARGS;
+
         N =         atoi(argv[2]);
         M =         N;
         L =         N;
@@ -89,6 +109,9 @@ int main(int argc, char const *argv[])
     }
     else if(argc == 8)
     {
+        if(std::string(argv[1]) == "double") dtype = true;
+        else if(std::string(argv[1]) == "int") dtype = false;
+        else return ERR_ARGS;
         M =         atoi(argv[2]);
         N =         atoi(argv[3]);
         L =         atoi(argv[4]);
@@ -100,20 +123,50 @@ int main(int argc, char const *argv[])
     {
         return ERR_ARGS;
     }
+    if( L < 1 || M < 1 || N < 1 ) return ERR_ARGS;
     if(dtype)
     {
-        doublematrix m1 = dread_matrix_from_file(file1, M, N); 
-        doublematrix m2 = dread_matrix_from_file(file2, N, L); 
+        doublematrix m1, m2;
+        try {
+            m1 = dread_matrix_from_file(file1, M, N); 
+            m2 = dread_matrix_from_file(file2, N, L); 
+        } 
+        catch (int e)
+        {
+            if(e == ERR_READ) return ERR_READ;
+            return ERR_SHAPE;
+        }
         doublematrix m3 = matrix_multiply(m1, m2);
-        write_matrix_to_file(file3.c_str(), m3);
+        try
+        {
+            write_matrix_to_file(file3.c_str(), m3);
+        }
+        catch (int e)
+        {
+            return ERR_WRITE;
+        }
     }
     else
     {
-        intmatrix m1, m2 , m3;
-        if(!iread_matrix_from_file(file1, M, N, &m1)) return 2;
-        if(!iread_matrix_from_file(file2, N, L, &m2)) return 2;
-        matrix_multiply(m1, m2, &m3);
-        write_matrix_to_file(file3.c_str(), m3);
+        intmatrix m1, m2;
+        try {
+            m1 = iread_matrix_from_file(file1, M, N); 
+            m2 = iread_matrix_from_file(file2, N, L); 
+        } 
+        catch (int e)
+        {
+            if(e == ERR_READ) return ERR_READ;
+            return ERR_SHAPE;
+        }
+        intmatrix m3 = matrix_multiply(m1, m2);
+        try
+        {
+            write_matrix_to_file(file3.c_str(), m3);
+        }
+        catch (int e)
+        {
+            return ERR_WRITE;
+        }
     }
     return 0;
 }
