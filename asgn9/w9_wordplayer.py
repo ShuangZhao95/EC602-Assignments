cebcc731763495a9171b4f5159ca5b9783ba99f5
Alexander Oleinik <alxndr@alxndr-mbp>
2016-11-11 13:20:29 -0500

    Even faster python

--- asgn9/w9_wordplayer.py
+++ asgn9/w9_wordplayer.py
@@ -1,57 +1,66 @@
 #!/usr/bin/env python3.5
-
-# 1. Load the file into a dictionary of lists using this procedure:
-#   a. Read a word "cat"
-#   b. Sort the letters of the word "act" 
-#   c. To the list at key "act" append "cat"
-# 2. Find all n letter combinations of string s in the dictionary (n=3, s=tacks)
-#   a. Sort the string "ackst"
-#   b. Generate a list of all 3 letter ordered combinations from the sorted string
-#       [ack, acs, act ...]
-#   c. Access the list at each key matching each of the combinations and append it 
-#       to the final word set
-# 3. Spit out the final word set
+# AUTHOR alex alxndr@bu.edu
 
 import sys
 import itertools
+import math
 
-def nCr(n,r):
-        f = math.factorial
-        return f(n) / f(r) / f(n-r)
+
+def nCr(n, r):
+    f = math.factorial
+    return f(n) / f(r) / f(n-r)
+
+
+def issubstr(small, large):
+    charlist = list(large)
+    for c in list(small):
+        if(c in charlist):
+            charlist.remove(c)
+        else:
+            return 0
+    return 1
+
 
 def main():
     filename = sys.argv[1]
-    
     wordlist = dict()
     f = open(filename)
 
     for w in f.read().split():
         s = ''.join(sorted(w))
-        wordlist.setdefault(s,[]).append(w)
+        wordlist.setdefault(len(s), {}).setdefault(s, []).append(w)
 
     while(1):
         inputstr = input()
         searchstr, length = inputstr.split()
-        print("got input")
         length = int(length)
         if(length == 0):
             break
         searchlist = sorted(searchstr)
-        print("making combinations")
-        combinations = [ set(x) for x in itertools.combinations(searchlist, length)]
-        print(combinations)
-        print("searching")
+        if(len(searchlist) < length):
+            print('.')
+            continue
         wordsfound = set()
-        for c in combinations:
-            w = ''.join(c)
-            if(w in wordlist):
-                wordlist[w]
-                for n in wordlist[w]:
-                    wordsfound.add(n)
+        if(wordlist.get(length)):
+            if(nCr(len(searchlist), length) > len(wordlist[length])):
+                for w in wordlist[length]:
+                    if(issubstr(w, searchlist)):
+                        for n in wordlist[length][w]:
+                            wordsfound.add(n)
+            else:
+                combinations = list()
+                for x in set(itertools.combinations(searchlist, length)):
+                    combinations.append(x)
+                for c in combinations:
+                    w = ''.join(c)
+                    if(w in wordlist[length]):
+                        for n in wordlist[length][w]:
+                            wordsfound.add(n)
 
-        for n in sorted(wordsfound) :
+        for n in sorted(wordsfound):
             print(n)
         print('.')
+    return 0
 
 if __name__ == "__main__":
     main()
