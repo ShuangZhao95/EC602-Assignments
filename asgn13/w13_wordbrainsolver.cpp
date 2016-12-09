ed57078f10e842f1412b3663c5625980a66d6001
Alexander Oleinik <alxndr@stormtrooper.vrmnet>
2016-12-09 00:01:05 -0500

    More work on w13

--- asgn13/w13_wordbrainsolver.cpp
+++ asgn13/w13_wordbrainsolver.cpp
@@ -16,7 +16,10 @@
  * =====================================================================================
  */
 #include <stdlib.h>
+#include <fstream>
+#define ALPHABETS 26
 using namespace std;
+
 /*
  * Gameboard Structure:
  * 4 e a v c
@@ -25,6 +28,16 @@ using namespace std;
  * 1 w e f v
  *   1 2 3 4
 */
+
+//A trie:[
+struct node
+{
+    public:
+        char me;
+        struct node *parent;
+        struct node *children[ALPHABETS];
+};
+/*
 struct puzzle
 {
     public:
@@ -47,17 +60,65 @@ int load_puzzle(puzzle *puz)
     puz->board[1] = {'c', 'a', 'p', 't'};
     puz->board[2] = {'n', 'i', 'b', 'a'};
     puz->board[3] = {'n', 'w', 'm', 'n'};
-    std::reverse(puz->board[0].begin(), puz->board[0].end());
-    std::reverse(puz->board[1].begin(), puz->board[1].end());
-    std::reverse(puz->board[2].begin(), puz->board[2].end());
-    std::reverse(puz->board[3].begin(), puz->board[3].end());
+    reverse(puz->board[0].begin(), puz->board[0].end());
+    reverse(puz->board[1].begin(), puz->board[1].end());
+    reverse(puz->board[2].begin(), puz->board[2].end());
+    reverse(puz->board[3].begin(), puz->board[3].end());
 }
 int solve_puzzle(puzzle *puz,                   //The puzzle board 
+                node trie,                      //The current location in the trie
                 vector<int> *checkvec,          //The list of possible words 
-                vector< pair<int, int> > sol,   //The list of cells used to solve
+                map< pair<int, int>, char > sol,   //The list of cells used to solve
                 int l,                          //The length of the search word
                 int cellx, int celly            //The coordinate of the current cell
                 )
 {
-    solve_puzzle(&puz, &checkvec, &sol, 
+    map< pair<int, int>, char > tsol = sol; 
+    tsol[make_pair(x, y)] = trie.me;
+    for(int x = (cellx -1 > 0) ? cellx -1: 0; 
+            x <= (cellx+1 < puz->dimension) ? cellx +1 : puz->dimension -1 ; 
+            x++)
+    {
+        for(int y = (celly -1 > 0) ? celly -1: 0; 
+                y <= (celly+1 < puz->dimension) ? celly +1 : puz->dimension -1 ; 
+                y++)
+        {
+            if(tsol[make_pair(x,y)]) continue;
+            try
+                char c = checkvec->at(x).at(y);
+            catch
+                continue;
+            if(trie->children[puz->at(x).at(y)]){
+                solve_puzzle(&puz, &(puz->at(x).at(y)), &tsol, l-1, x, y); 
+            }
+        }
+    }
+}
+*/
+int load_dictionary(string fn, node *trie)
+{ 
+    ifstream infile(fn);
+    string s;
+    int count;
+    while( infile >> s )
+    {
+        count++;
+        node * traverse = trie;
+        if(trie->children[s.length()] == NULL)
+            trie->children[s.length()] = (node){'0'+s.length(), traverse,NULL};
+        traverse = traverse->children[s.length()];
+        for(auto c: s)
+        {
+            if(traverse->children[c] == NULL)
+                traverse->children[c] = (node){c, traverse};
+            traverse = traverse->children[c];
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    node trie;
+    load_dictionary("small_word_list.txt", &trie);
 }
